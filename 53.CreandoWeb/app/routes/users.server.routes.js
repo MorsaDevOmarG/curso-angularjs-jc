c15b722439aac49911099b722b10bd3e96c48d01
Omar Garcia <omar.garcia@morsa.com.mx>
2025-02-06 00:06:01 -0600

    Metodo FIND

--- 53.CreandoWeb/app/routes/users.server.routes.js
+++ 53.CreandoWeb/app/routes/users.server.routes.js
@@ -3,5 +3,5 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-    app.route('/users').post(users.create);
+    app.route('/users').get(users.list).post(users.create);
 }
