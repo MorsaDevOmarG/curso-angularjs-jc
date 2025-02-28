3810dcf730dc3787b458fd6d8d87dbcccb075958
Omar Garcia <omar.garcia@morsa.com.mx>
2025-02-27 22:28:37 -0600

    CRear sistema de autentificacion

--- 53.CreandoWeb/config/express.js
+++ 53.CreandoWeb/config/express.js
@@ -4,7 +4,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     config = require('./config'),
-    session = require('express-session');
+    session = require('express-session'),
+    passport = require('passport');
 
 module.exports = function() {
     var app = express();
@@ -27,6 +28,9 @@ module.exports = function() {
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     require('../app/routes/index.server.routes.js')(app);
     require('../app/routes/users.server.routes.js')(app);
