59ee48522a24d928ab24d09f724f4952183a85c8
Omar Garcia <omar.garcia@morsa.com.mx>
2025-02-27 22:13:00 -0600

    Accediendo a un documento por su ID

--- 53.CreandoWeb/app/controllers/users.server.controller.js
+++ 53.CreandoWeb/app/controllers/users.server.controller.js
@@ -17,11 +17,31 @@ exports.create = function(req, res, next) {
 };
 
 exports.list = function(req, res, next) {
-    User.find({}, function(err, users) {
+    User.find({}, 'username email', function(err, users) {
         if (err) {
             return next(err);
         } else {
             res.json(users);
         }
     });
+};
+
+exports.read = function(req, res) {
+    res.json(req.user);
+};
+
+exports.userById = function(req, res, next, id) {
+    User.findOne(
+        {
+            _id: id
+        }, function (err, user) {
+            if (err) {
+                return next(err);
+            } else {
+                return next(new Error('Failed to load user ' + id));
+                req.user = user;
+                next();
+            }
+        }
+    );
 };
--- 53.CreandoWeb/app/routes/users.server.routes.js
+++ 53.CreandoWeb/app/routes/users.server.routes.js
@@ -4,4 +4,8 @@ var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
     app.route('/users').get(users.list).post(users.create);
+
+    // Los 2 puntos :, indica que se manejará como un parámetro...
+    app.route('/users/:userId').get(users.read);
+        app.param('userId', users.userById);
 }
